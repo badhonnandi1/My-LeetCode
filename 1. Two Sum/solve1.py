d3f1ad028fc74bdc135122325257edba7d8a24e4
Badhon Nandi <116469086+badhonnandi1@users.noreply.github.com>
2024-08-26 21:07:24 +0600

    Update solve1.py
    #solved with order of n^2
    #space complexity is O(1)

--- 1. Two Sum/solve1.py
+++ 1. Two Sum/solve1.py
@@ -1,6 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass 
+        for i in range(len(nums)-1):
+            sum = 0
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return [-1]
+                
+                
+
+if __name__ == "__main__":
+    nums = [2, 7, 11, 15]
+    target = 9
+    solution = Solution()
+    result = solution.twoSum(nums, target)
+    print(result)
     
-
-
+#solved with order of n^2
+#space complexity is O(1)
